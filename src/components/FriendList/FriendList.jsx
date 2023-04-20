17c77ce5d1ab81e1307105cb48c283dc5d83139e
Serhii Izotov <sergiyizotov@gmail.com>
2023-04-20 16:27:00 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,7 +3,6 @@ import { FriendListItem } from './FriendListItem/FriendListItem';
 import { FriendsList } from './FriendList.styled';
 
 export const FriendList = ({ friends }) => {
-  console.log(friends);
   return (
     <FriendsList>
       {friends.map(({ avatar, name, isOnline, id }) => (
@@ -20,6 +19,11 @@ export const FriendList = ({ friends }) => {
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
-    PropTypes.shape({ id: PropTypes.number.isRequired })
-  ),
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
 };
